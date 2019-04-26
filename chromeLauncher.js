04f2ed9ce1e6a6207750329f41ad8d43695993b1
Jirateep Suranant <jirateep@th.ibm.com>
2019-04-26 12:22:12 +0700

    update chrome launcher

--- chromeLauncher.js
+++ chromeLauncher.js
@@ -16,7 +16,7 @@ const browserWSEndpoint = process.env.wsURL || null;
     console.log('1. Quit this script (cmd/ctrl+C).');
     console.log('2. Chrome will still be running.');
     console.log('4. Re-return the script with:');
-    console.log(`wsURL=${browser.wsEndpoint()} username=[add secret user here] node attacher.js`);
+    console.log(`$env:wsURL="${browser.wsEndpoint()}" $env=username="[add secret user here]" node attacher03.js`);
     console.log('5. Puppeteer will reconnect to the existing Chrome instead of launching a new browser.');
     return;
   }
